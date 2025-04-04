62e5c4de766c6429fb30a2630930b048094761ba
OrniceDev <ornihouss1@gmail.com>
2025-04-04 17:09:55 +0100

    feat(agenda): MAJ Agenda

---
+++ app/Models/Agenda.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Agenda extends Model
+{
+    use HasFactory;
+    protected $table ='tbl_agendas';
+    protected $fillable = [
+        'title',
+        'description',
+        'date',
+        'patient_id',
+        'user_id',
+    ];
+}
